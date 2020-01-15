c791df500e8389e6283b56997e61c1beba20b6c1
danielwangai <mainad57@gmail.com>
2020-01-15 12:11:51 +0300

    define the structure of a cell
    - set default multiplier for each cell
    - refactor board to be an array of cells

--- scrabble.go
+++ scrabble.go
@@ -1,20 +1,40 @@
 package main
 
-var Board [][]string
+type Cell struct {
+	Row        int
+	Column     int
+	Letter     string
+	Multiplier int
+}
 
+var Board [][]Cell
+
+// assign point to cells
 func init() {
 	makeBoard()
 }
 
+// set default multiplier
+// the minimum multiplier value for a cell is 1
+// unless the case of a blank where we multiply by 0
+func (c *Cell) init() {
+	c.Multiplier = 1
+}
+
+// generate the 2D(1%X15) grid/board
 func makeBoard() {
-	count := 15
-	for count > 0 {
-		row := []string{}
+	count := 0
+	for count < 15 {
+		row := make([]Cell, 15)
 		for i := 0; i < 15; i++ {
-			row = append(row, "")
+			cell := &Cell{}
+			cell.init()
+			cell.Row = count
+			cell.Column = i
+			row[i] = *cell
 		}
 		Board = append(Board, row)
-		count--
+		count++
 	}
 }
 
