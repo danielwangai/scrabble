fcf28046715d31224dc5f46b3fc4d5708ad43c3f
danielwangai <mainad57@gmail.com>
2020-01-20 11:56:49 +0300

    set letter points and count

--- scrabble.go
+++ scrabble.go
@@ -1,13 +1,51 @@
 package main
 
+import "fmt"
+
 type Cell struct {
 	Row        int
 	Column     int
-	Letter     string
+	Letter     map[string]*LetterDetail
 	Multiplier int
 }
 
 var Board [][]Cell
+
+type LetterDetail struct {
+	Points int
+	Count  int
+}
+
+// Letters and corresponding count and points
+var Letters = map[string]LetterDetail{
+	"A":     LetterDetail{Points: 1, Count: 9},
+	"B":     LetterDetail{Points: 3, Count: 2},
+	"C":     LetterDetail{Points: 3, Count: 2},
+	"D":     LetterDetail{Points: 2, Count: 4},
+	"E":     LetterDetail{Points: 1, Count: 12},
+	"F":     LetterDetail{Points: 4, Count: 2},
+	"G":     LetterDetail{Points: 2, Count: 3},
+	"H":     LetterDetail{Points: 4, Count: 2},
+	"I":     LetterDetail{Points: 1, Count: 9},
+	"J":     LetterDetail{Points: 8, Count: 1},
+	"K":     LetterDetail{Points: 5, Count: 1},
+	"L":     LetterDetail{Points: 1, Count: 4},
+	"M":     LetterDetail{Points: 3, Count: 2},
+	"N":     LetterDetail{Points: 1, Count: 6},
+	"O":     LetterDetail{Points: 1, Count: 8},
+	"P":     LetterDetail{Points: 3, Count: 2},
+	"Q":     LetterDetail{Points: 10, Count: 1},
+	"R":     LetterDetail{Points: 1, Count: 6},
+	"S":     LetterDetail{Points: 1, Count: 4},
+	"T":     LetterDetail{Points: 1, Count: 6},
+	"U":     LetterDetail{Points: 1, Count: 4},
+	"V":     LetterDetail{Points: 4, Count: 2},
+	"W":     LetterDetail{Points: 4, Count: 2},
+	"X":     LetterDetail{Points: 8, Count: 1},
+	"Y":     LetterDetail{Points: 4, Count: 2},
+	"Z":     LetterDetail{Points: 10, Count: 1},
+	"Blank": LetterDetail{Points: 0, Count: 2},
+}
 
 // assign point to cells
 func init() {
@@ -38,6 +76,7 @@ func makeBoard() {
 	}
 }
 
-// func main() {
-// 	fmt.Println(len(Board[0]))
-// }
+func main() {
+	// fmt.Println(len(Board[0]))
+	fmt.Println(Letters)
+}
